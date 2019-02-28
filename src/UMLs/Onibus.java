387a10595c7a04fa767005eafa1225fc2f1990ce
Lucas P. Fernandes <lucas@substractum.com.br>
2019-02-28 13:18:03 -0300

    Onibus e sala de aula

---
+++ src/UMLs/Onibus.java
@@ -0,0 +1,63 @@
+package UMLs;
+
+public class Onibus {
+	private int quantidadeBancos;
+	private String tipoDeRoda;
+	private String tipoDeVolante;
+	private String tipoDeMotor;
+	
+	public void andar() {
+		
+	}
+	
+	public void parar() {
+		
+	}
+	
+	public void virarEsquerda() {
+		
+	}
+	
+	public void virarDireita() {
+		
+	}
+	
+	public void darRe() {
+		
+	}
+
+	public int getQuantidadeBancos() {
+		return quantidadeBancos;
+	}
+
+	public void setQuantidadeBancos(int quantidadeBancos) {
+		this.quantidadeBancos = quantidadeBancos;
+	}
+
+	public String getTipoDeRoda() {
+		return tipoDeRoda;
+	}
+
+	public void setTipoDeRoda(String tipoDeRoda) {
+		this.tipoDeRoda = tipoDeRoda;
+	}
+
+	public String getTipoDeVolante() {
+		return tipoDeVolante;
+	}
+
+	public void setTipoDeVolante(String tipoDeVolante) {
+		this.tipoDeVolante = tipoDeVolante;
+	}
+
+	public String getTipoDeMotor() {
+		return tipoDeMotor;
+	}
+
+	public void setTipoDeMotor(String tipoDeMotor) {
+		this.tipoDeMotor = tipoDeMotor;
+	}
+	
+	
+	
+}
--- src/UMLs/SalaDeAula.java
+++ src/UMLs/SalaDeAula.java
@@ -1,8 +1,44 @@
 package UMLs;
 
 public class SalaDeAula extends Aluno {
-	
-	private int quantidadeAlunos;
+
 	private boolean professorVeio;
+	private String aula;
+	private String tipoDeQuadro;
+	private int quantidadeMesas;
+	private int quantidadeCadeiras;
+	
+	
+	
+	public String getTipoDeQuadro() {
+		return tipoDeQuadro;
+	}
+	public void setTipoDeQuadro(String tipoDeQuadro) {
+		this.tipoDeQuadro = tipoDeQuadro;
+	}
+	public int getQuantidadeMesas() {
+		return quantidadeMesas;
+	}
+	public void setQuantidadeMesas(int quantidadeMesas) {
+		this.quantidadeMesas = quantidadeMesas;
+	}
+	public int getQuantidadeCadeiras() {
+		return quantidadeCadeiras;
+	}
+	public void setQuantidadeCadeiras(int quantidadeCadeiras) {
+		this.quantidadeCadeiras = quantidadeCadeiras;
+	}
+	public boolean isProfessorVeio() {
+		return professorVeio;
+	}
+	public void setProfessorVeio(boolean professorVeio) {
+		this.professorVeio = professorVeio;
+	}
+	public String getAula() {
+		return aula;
+	}
+	public void setAula(String aula) {
+		this.aula = aula;
+	}
 	
 }
